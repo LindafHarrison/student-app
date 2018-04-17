b96dd9bd5067608f9718267e7d774cebef317cab
Linda Harrison <lindafaithharrison@gmail.com>
2018-04-17 14:21:20 -0400

    connected to db

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const express = require("express")
 const app = express()
 const path = require('path')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
+
+mongoose.connect('mongodb://user:password@ds147589.mlab.com:47589/lindasstudents').then(() => console.log("connected")).catch((err) => {
+   console.log(err)
+})
 
 app.get("/", (req,res) => {
     res.sendFile(path.join(__dirname, 'index.html'))
